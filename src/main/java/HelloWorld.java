a6fcd093095ed9a98c242f3e6585845068304038
CyberfuzZ <keepthelight@icloud.com>
2021-10-07 15:53:50 +0300

    hello world - 1

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Класс HelloWorld
+ *
+ * @author Evgeniy Zaytsev
+ * @version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
